e92e4fe15003c67d4005a4d14cb45e43aa464ecd
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-01-25 06:32:35 +0000

    Add label repository

---
+++ my-todo/src/repositories/label.rs
@@ -0,0 +1,126 @@
+use axum::async_trait;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+
+use super::RepositoryError;
+
+#[async_trait]
+pub trait LabelRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+	async fn create(&self, name: String) -> anyhow::Result<Label>;
+	async fn all(&self) -> anyhow::Result<Vec<Label>>;
+	async fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, sqlx::FromRow)]
+pub struct Label {
+	pub id: i32,
+	pub name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct UpdateLable {
+	id: i32,
+	name: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct LabelRepositoryForDb {
+	pool: PgPool,
+}
+
+impl LabelRepositoryForDb {
+	pub fn new(pool: PgPool) -> Self {
+		Self { pool }
+	}
+}
+
+#[async_trait]
+impl LabelRepository for LabelRepositoryForDb {
+	async fn create(&self, name: String) -> anyhow::Result<Label> {
+		let optional_label = sqlx::query_as::<_, Label>(
+			r#"
+				select * from labels where name = $1
+			"#,
+		)
+		.bind(name.clone())
+		.fetch_optional(&self.pool)
+		.await?;
+
+		if let Some(label) = optional_label {
+			return Err(RepositoryError::Duplicate(label.id))
+		}
+
+		let label = sqlx::query_as::<_, Label>(
+			r#"
+			INSERT INTO labels (name) VALUES ($1)
+			returning *
+			"#,
+		)
+		.bind(name.clone())
+		.fetch_one(&self.pool)
+		.await?;
+
+		Ok(label)
+	}
+
+	async fn all(&self) -> anyhow::Result<Vec<Label>> {
+		let labels = sqlx::query_as::<_, Label>(
+			r#"
+			select * from labels
+			order by labels.id asc;
+			"#,
+		)
+		.fetch_all(&self.pool)
+		.await?;
+
+		Ok(labels)
+	}
+
+	async fn delete(&self, id: i32) -> anyhow::Result<()> {
+		sqlx::query(
+			r#"
+			delete from labels where id = $1
+			"#,
+		)
+		.bind(id)
+		.execute(&self.pool)
+		.await
+		.map_err(|e| match e {
+			sqlx::Error::RowNotFound => RepositoryError::NotFound(id),
+			_ => RepositoryError::Unexpected(e.to_string()),
+		})?;
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+#[cfg(feature = "database-test")]
+mod test {
+	use super::*;
+	use dotenv::dotenv;
+	use sqlx::PgPool;
+	use std::env;
+
+	#[tokio::test]
+	async fn crud_scenario() {
+		dotenv().ok();
+		let database_url = &env::var("DATABASE_URL").expect("undifined [DATABASE_URL]");
+		let pool = PgPool::connect(database_url).await.expect(&format!("fail connect database, url is [{}]", database_url));
+		
+		// create
+		let repository = LabelRepositoryForDb::new(pool);
+		let label_text = "test_label";
+
+		let label = repository.create(label_text.to_string()).await.expect("[create] returned Err");
+		assert_eq!(label.name, label_text);
+
+		// all
+		let labels = repository.all().await.expect("[all] returned Err");
+		let label = labels.last().unwrap();
+		assert_eq!(label.name, label_text);
+
+		// delete
+		repository.delete(label.id).await.expect("[delete] returned Err");
+	}
+}
